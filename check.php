20895726750b2ed17e2b4a2bc2c2a65baa50225f
Rajan Poudel <raajan.works@gmail.com>
2025-05-08 21:53:01 +0545

    Update check.php

--- check.php
+++ check.php
@@ -3,13 +3,16 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-   $password = $_POST['password'];
+    $password = $_POST['password'];
     $confirm_password = $_POST['cpassword'];
+    
+    
    if(isset($_POST['remember'])) {
         $remember = $_POST['remember'];
-    } else {
-        $remember = false;
-    }
+    } 
+          else {
+          $remember = false;
+                }
 
  
         $filename = 'data.txt';
@@ -26,15 +29,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
         setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
         echo "Data Is Saved And Cookie Is Set!!! It Happen Due To You Have Checked Remember Me........<br>";
     } 
+    
+    
     else if($file && $remember==false) {
         $data = "Name: $name, Email: $email, Password: $password, Confirm Password: $confirm_password\n";
         fwrite($file, $data);
         fclose($file);
         echo "Data Is Saved. It Happen Due To You Have Not Checked Remember Me........<br>";
     }
+    
+    
     else {
         echo "Error opening file!";
     }
+    
+    
 }
 
 
